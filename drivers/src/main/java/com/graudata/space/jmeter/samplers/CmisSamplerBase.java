a228ed22b8dea944d14d20390535fb06e15ce13a
Frederick Roth <f-roth@megaera.de>
2014-01-28 15:16:34 +0100

    Added Samplerbase

--- drivers/src/main/java/com/graudata/space/jmeter/samplers/CmisSamplerBase.java
+++ drivers/src/main/java/com/graudata/space/jmeter/samplers/CmisSamplerBase.java
@@ -8,15 +8,13 @@ import org.apache.jmeter.samplers.SampleResult;
 
 import com.graudata.space.jmeter.drivers.JmeterUtil;
 
+public abstract class CmisSamplerBase extends AbstractJavaSamplerClient {
 
-public abstract class CmisSamplerBase extends AbstractJavaSamplerClient
-{
-    
-    protected Session session; 
+    protected Session session;
 
     @Override
     public Arguments getDefaultParameters() {
-        Arguments args = super.getDefaultParameters();
+        Arguments args = new Arguments();
         args.addArgument("host", "https://demo.dataspace.cc/");
         args.addArgument("user", "user");
         args.addArgument("pw", "****");
@@ -25,8 +23,8 @@ public abstract class CmisSamplerBase extends AbstractJavaSamplerClient
 
     @Override
     public SampleResult runTest(JavaSamplerContext ctx) {
-        
-        if(session == null) {
+
+        if (session == null) {
             SampleResult result = new SampleResult();
             result.setResponseCode("Not Authorized");
             result.setResponseData("Unable to get Session", "UTF-8");
@@ -34,15 +32,16 @@ public abstract class CmisSamplerBase extends AbstractJavaSamplerClient
         }
 
         return doRunTest(ctx);
-        
+
     }
-    
+
     public abstract SampleResult doRunTest(JavaSamplerContext ctx);
 
     @Override
     public void setupTest(JavaSamplerContext ctx) {
         try {
-            session = JmeterUtil.getSession(ctx.getParameter("host"), ctx.getParameter("user"), ctx.getParameter("pw"));
+            session = JmeterUtil.getSession(ctx.getParameter("host"),
+                    ctx.getParameter("user"), ctx.getParameter("pw"));
         } catch (Exception e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
