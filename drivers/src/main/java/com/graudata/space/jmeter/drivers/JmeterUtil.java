66a4d5efeb1fddedf3dcfaaf41712d672aae8360
Frederick Roth <f-roth@megaera.de>
2014-01-28 15:16:02 +0100

    Added A lot of helper functions for load drivers

--- drivers/src/main/java/com/graudata/space/jmeter/drivers/JmeterUtil.java
+++ drivers/src/main/java/com/graudata/space/jmeter/drivers/JmeterUtil.java
@@ -1,17 +1,36 @@
 package com.graudata.space.jmeter.drivers;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
+import java.util.UUID;
 
+import org.apache.chemistry.opencmis.client.api.CmisObject;
+import org.apache.chemistry.opencmis.client.api.FileableCmisObject;
+import org.apache.chemistry.opencmis.client.api.Folder;
 import org.apache.chemistry.opencmis.client.api.Repository;
 import org.apache.chemistry.opencmis.client.api.Session;
 import org.apache.chemistry.opencmis.client.api.SessionFactory;
+import org.apache.chemistry.opencmis.client.api.Tree;
 import org.apache.chemistry.opencmis.client.runtime.SessionFactoryImpl;
+import org.apache.chemistry.opencmis.commons.PropertyIds;
 import org.apache.chemistry.opencmis.commons.SessionParameter;
+import org.apache.chemistry.opencmis.commons.data.ContentStream;
+import org.apache.chemistry.opencmis.commons.enums.BaseTypeId;
 import org.apache.chemistry.opencmis.commons.enums.BindingType;
+import org.apache.chemistry.opencmis.commons.enums.UnfileObject;
+import org.apache.chemistry.opencmis.commons.enums.VersioningState;
+import org.apache.chemistry.opencmis.commons.impl.dataobjects.ContentStreamImpl;
 
 public class JmeterUtil {
+    public static String randomName() {
+        return UUID.randomUUID().toString();
+    }
+
     public static Session getSession(String host, String user, String pw)
             throws Exception {
         // default factory implementation
@@ -36,5 +55,89 @@ public class JmeterUtil {
         }
         throw new Exception();
 
+    }
+
+    public static void createRecursiveFoldersAndFiles(Session session,
+            String base, int depth, long folderNum, long fileNum, long fileSize) {
+        Map<String, Object> properties = new HashMap<>();
+        properties.put(PropertyIds.OBJECT_TYPE_ID, "cmis:folder");
+        properties.put(PropertyIds.NAME, base);
+
+        Folder root = session.getRootFolder().createFolder(properties);
+        createRecursiveFoldersAndFiles(root, depth, folderNum, fileNum,
+                fileSize);
+    }
+
+    private static void createRecursiveFoldersAndFiles(Folder base, int depth,
+            long folderNum, long fileNum, long fileSize) {
+        for (int i = 0; i < fileNum; i++) {
+            createDoc(base, fileSize);
+        }
+        if (depth > 0) {
+            for (int i = 0; i < fileNum; i++) {
+                Map<String, Object> properties = new HashMap<>();
+                properties.put(PropertyIds.OBJECT_TYPE_ID, "cmis:folder");
+                properties.put(PropertyIds.NAME, randomName());
+                Folder subfolder = base.createFolder(properties);
+                createRecursiveFoldersAndFiles(subfolder, depth - 1, folderNum,
+                        fileNum, fileSize);
+            }
+        }
+
+    }
+
+    private static void createDoc(Folder base, long fileSize) {
+        String name = randomName();
+        Map<String, Object> properties = new HashMap<>();
+        properties.put(PropertyIds.OBJECT_TYPE_ID, "cmis:document");
+        properties.put(PropertyIds.NAME, name);
+
+        // content
+        Random random = new Random();
+        byte[] content = new byte[(int) fileSize];
+        random.nextBytes(content);
+        InputStream stream = new ByteArrayInputStream(content);
+        ContentStream contentStream = new ContentStreamImpl(name,
+                BigInteger.valueOf(content.length), "text/plain", stream);
+
+        // create a major version
+        base.createDocument(properties, contentStream, VersioningState.MAJOR);
+    }
+
+    public static void deleteFolderRecursive(Session session, String base) {
+        CmisObject object;
+        object = findChildByName(session.getRootFolder(), base);
+        if (null != object && object.getBaseTypeId() == BaseTypeId.CMIS_FOLDER) {
+            Folder folder = (Folder) object;
+            folder.deleteTree(true, UnfileObject.DELETE, true);
+        }
+
+    }
+
+    public static CmisObject findChildByName(Folder folder, String base) {
+        for (CmisObject child : folder.getChildren()) {
+            if (child.getName().equals(base)) {
+                return child;
+            }
+        }
+        return null;
+    }
+    
+    public static long countDescendants(Folder folder, int depth) {
+        long count = 0;
+        for(Tree<FileableCmisObject> tree : folder.getDescendants(depth)){
+            count +=1;
+            count += countDescendants(tree);
+        }
+        return count;
+    }
+    
+    private static long countDescendants(Tree<FileableCmisObject> tree) {
+        long count = 0;
+        for(Tree<FileableCmisObject> subtree : tree.getChildren()){
+            count += 1;
+            count += countDescendants(subtree);
+        }
+        return count;
     }
 }
--- drivers/src/test/java/com/graudata/space/UtilTest.java
+++ drivers/src/test/java/com/graudata/space/UtilTest.java
@@ -1,24 +1,90 @@
 package com.graudata.space;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import org.apache.chemistry.opencmis.client.api.CmisObject;
+import org.apache.chemistry.opencmis.client.api.Document;
+import org.apache.chemistry.opencmis.client.api.Folder;
 import org.apache.chemistry.opencmis.client.api.Session;
+import org.apache.chemistry.opencmis.commons.enums.BaseTypeId;
+import org.junit.After;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.graudata.space.jmeter.drivers.JmeterUtil;
 
+@SuppressWarnings("static-method")
 public class UtilTest {
     private static String HOST = System.getProperty("test.host");
     private static String USER = System.getProperty("test.user");
     private static String PASSWORD = System.getProperty("test.password");
 
-    @SuppressWarnings("static-method")
-    @Test
-    public void testApp() throws Exception
-    {
+    private static String TESTFOLDER = "testfolder";
 
-            Session session = JmeterUtil.getSession(HOST, USER, PASSWORD);
-            assertTrue(session.getRootFolder().isRootFolder());
-
+    @After
+    public void tearDown() throws Exception {
+        Session session = JmeterUtil.getSession(HOST, USER, PASSWORD);
+        JmeterUtil.deleteFolderRecursive(session, TESTFOLDER);
     }
+
+    @Test
+    public void testGetSession() throws Exception {
+        Session session = JmeterUtil.getSession(HOST, USER, PASSWORD);
+        assertTrue(session.getRootFolder().isRootFolder());
+    }
+    
+    @Test
+    @Ignore
+    public void testCreateFolderTreeBigFile() throws Exception {
+        long filesize = 1024*1024*1024;
+        int depth = 0;
+        long filenum = 1;
+        long foldernum = 0;
+        Session session = JmeterUtil.getSession(HOST, USER, PASSWORD);
+        JmeterUtil.createRecursiveFoldersAndFiles(session, TESTFOLDER, depth,
+                foldernum, filenum, filesize);
+
+        assertStructureCreated(filesize, depth, filenum, foldernum, session);
+    }
+
+    @Test
+    public void testCreateFolderTree() throws Exception {
+        long filesize = 1;
+        int depth = 2;
+        long filenum = 2;
+        long foldernum = 2;
+        Session session = JmeterUtil.getSession(HOST, USER, PASSWORD);
+        JmeterUtil.createRecursiveFoldersAndFiles(session, TESTFOLDER, depth,
+                foldernum, filenum, filesize);
+
+        assertStructureCreated(filesize, depth, filenum, foldernum, session);
+    }
+
+    private void assertStructureCreated(long filesize, int depth, long filenum,
+            long foldernum, Session session) {
+        CmisObject object = JmeterUtil.findChildByName(session.getRootFolder(),
+                TESTFOLDER);
+        assertNotNull("Folder not created", object);
+        assertEquals(BaseTypeId.CMIS_FOLDER, object.getBaseTypeId());
+        Folder folder = (Folder) object;
+        assertEquals(filenum + foldernum, folder.getChildren()
+                .getTotalNumItems());
+
+        for (CmisObject obj : folder.getChildren()) {
+            if (BaseTypeId.CMIS_DOCUMENT == obj.getBaseTypeId()) {
+                assertEquals(filesize,
+                        ((Document) obj).getContentStreamLength());
+                break;
+            }
+        }
+        long filesInRoot = filenum;
+        long subfolders =  foldernum*(long)(Math.pow(foldernum, depth)-1)/(foldernum-1);
+        long desc = JmeterUtil.countDescendants(folder, depth+1);
+        assertEquals(filesInRoot + subfolders * (1 + filenum), desc);
+    }
+
+
+
 }
