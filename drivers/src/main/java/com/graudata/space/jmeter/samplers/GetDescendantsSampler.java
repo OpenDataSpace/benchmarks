63173cac1286fd4dd0e6b1fa4abd1e580ebae903
Frederick Roth <f-roth@megaera.de>
2014-01-28 15:17:16 +0100

    Adds DescendantsSampler

---
+++ drivers/src/main/java/com/graudata/space/jmeter/samplers/GetDescendantsSampler.java
@@ -0,0 +1,38 @@
+package com.graudata.space.jmeter.samplers;
+
+import org.apache.chemistry.opencmis.client.api.CmisObject;
+import org.apache.chemistry.opencmis.client.api.Folder;
+import org.apache.jmeter.config.Arguments;
+import org.apache.jmeter.protocol.java.sampler.JavaSamplerContext;
+import org.apache.jmeter.samplers.SampleResult;
+
+import com.graudata.space.jmeter.drivers.JmeterUtil;
+
+public class GetDescendantsSampler extends CmisSamplerBase {
+
+    private static final String DEPTH = "depth";
+    private static final String BASE = "basename";
+
+    @Override
+    public Arguments getDefaultParameters() {
+        Arguments args = super.getDefaultParameters();
+        args.addArgument(DEPTH, "10", "Depth of folder scannign");
+        args.addArgument(BASE, "test", "Name of the BaseFolder");
+        return args;
+    }
+
+    @Override
+    public SampleResult doRunTest(JavaSamplerContext ctx) {
+        String base = ctx.getParameter(BASE);
+        int depth = Integer.parseInt(ctx.getParameter(DEPTH));
+        
+        SampleResult result = new SampleResult();
+        result.sampleStart();
+        CmisObject obj = JmeterUtil.findChildByName(session.getRootFolder(), base);
+        long desc = JmeterUtil.countDescendants((Folder) obj, depth);
+        result.sampleEnd();
+        result.setResponseMessage("count" + desc);
+        result.setResponseOK();
+        return result;
+    }
+}
