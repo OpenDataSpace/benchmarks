cc08587253cc3032c87079f0e45fcb3c5a2c77c4
Frederick Roth <f-roth@megaera.de>
2014-07-24 14:45:18 +0200

    changescript

---
+++ python/changeallfiles.py
@@ -0,0 +1,22 @@
+import os
+import sys
+
+def append1kbtoFile(filename):
+    print(filename)
+    fp = open(filename, "ab")
+    fp.write(bytes('a' * 1024 ,"ASCII"))
+    fp.close()
+
+base = "."
+
+if(len(sys.argv) > 1):
+    base = sys.argv[1];
+
+
+level1 = base;
+for level2 in range (0, 10):
+    dirpath = os.path.join(level1, "b" + str(level2))
+    for filename in range (0, 10):
+        append1kbtoFile(os.path.join(dirpath, "file" + str(filename)))
+
+
