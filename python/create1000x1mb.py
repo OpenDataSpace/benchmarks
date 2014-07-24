baa2a2ff90c292a06c6d72c3f154b23d0aa6eb4b
Frederick Roth <f-roth@megaera.de>
2014-07-24 11:59:49 +0200

    Added python script

---
+++ python/create1000x1mb.py
@@ -0,0 +1,15 @@
+import os
+
+def create1mbFile(filename):
+	fp = open(filename, "wb")
+	fp.write(bytes('\0' * 1024 * 1024 ,"ASCII"))
+	fp.close()
+
+for level1 in range (0, 10):
+	for level2 in range (0, 10):
+			dirpath = os.path.join("a" + str(level1), "b" + str(level2))
+			os.makedirs(dirpath)
+			for filename in range (0, 10):
+				create1mbFile(os.path.join(dirpath, "file" + str(filename)))
+
+
