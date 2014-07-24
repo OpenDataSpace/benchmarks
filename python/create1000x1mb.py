c2e0db5867de3ae43dc9abab6b242988005fa982
Frederick Roth <f-roth@megaera.de>
2014-07-24 14:33:49 +0200

    Adds command line arg that specifies basepath

--- python/create1000x1mb.py
+++ python/create1000x1mb.py
@@ -1,15 +1,22 @@
 import os
+import sys
 
 def create1mbFile(filename):
-	fp = open(filename, "wb")
-	fp.write(bytes('\0' * 1024 * 1024 ,"ASCII"))
-	fp.close()
+    fp = open(filename, "wb")
+    fp.write(bytes('\0' * 1024 * 1024 ,"ASCII"))
+    fp.close()
+
+base = "."
+
+if(len(sys.argv) > 1):
+    base = sys.argv[1];
+
 
 for level1 in range (0, 10):
-	for level2 in range (0, 10):
-			dirpath = os.path.join("a" + str(level1), "b" + str(level2))
-			os.makedirs(dirpath)
-			for filename in range (0, 10):
-				create1mbFile(os.path.join(dirpath, "file" + str(filename)))
+    for level2 in range (0, 10):
+        dirpath = os.path.join(base, "a" + str(level1), "b" + str(level2))
+        os.makedirs(dirpath)
+        for filename in range (0, 10):
+            create1mbFile(os.path.join(dirpath, "file" + str(filename)))
 
 
